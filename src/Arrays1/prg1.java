bea6cf793c42467ed030b67c92b7ae04697592b9
UTSAV JAIN <utsav7011@gmail.com>
2023-02-24 10:21:54 +0530

    Initial commit

---
+++ src/Arrays1/prg1.java
@@ -0,0 +1,22 @@
+package Arrays1;
+import java.util.*;
+public class prg1 {
+    public static void main(String[] args){
+        System.out.print("Helloooo IDE");
+
+        int [] arr = {1,2,3,4,6,7,8,9};
+        for (int ele : arr){
+            System.out.print(ele + "   ");
+        }
+
+//        multidimensional Array:
+
+        int [] marks;
+        int [][] flats;
+        flats =  new int [2][3];
+        
+
+
+    }
+
+}
